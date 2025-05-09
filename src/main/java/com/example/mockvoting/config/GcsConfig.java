abf98d660cb8b635527eb9b4c75d48cb6f9b5c8c
YupBokchi <booo7575@naver.com>
2025-05-09 15:37:23 +0900

    Fix/GcsConfig

--- src/main/java/com/example/mockvoting/config/GcsConfig.java
+++ src/main/java/com/example/mockvoting/config/GcsConfig.java
@@ -3,31 +3,25 @@ package com.example.mockvoting.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class GcsConfig {
 
+    @Value("${gcs.key.location}")
+    private String keyPath;
+
     @Bean
     public Storage storage() throws IOException {
-        String keyPath = System.getenv("GOOGLE_APPLICATION_CREDENTIALS");
-
-        GoogleCredentials credentials;
-        if (keyPath != null && !keyPath.isBlank()) {
-            credentials = GoogleCredentials.fromStream(new FileInputStream(keyPath)); // ✅ GCP Cloud Run용
-        } else {
-            // fallback: 로컬 개발용 (resources 아래 경로)
-            credentials = GoogleCredentials.fromStream(
-                    new ClassPathResource("gcs-key.json").getInputStream()
-            );
-        }
-
+        FileInputStream serviceAccount = new FileInputStream(keyPath);
+        GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
         return StorageOptions.newBuilder().setCredentials(credentials).build().getService();
     }
-
 }
