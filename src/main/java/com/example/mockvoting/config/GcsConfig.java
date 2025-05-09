1ba6bb894b7d6b1619959c673e229c4ee474547f
YupBokchi <booo7575@naver.com>
2025-05-09 14:23:20 +0900

    Fix/GcsPath

--- src/main/java/com/example/mockvoting/config/GcsConfig.java
+++ src/main/java/com/example/mockvoting/config/GcsConfig.java
@@ -3,23 +3,25 @@ package com.example.mockvoting.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 
 import java.io.FileInputStream;
 import java.io.IOException;
 
-
 @Configuration
 public class GcsConfig {
 
-    @Value("${gcs.key.location}")
-    private String keyPath;
-
     @Bean
     public Storage storage() throws IOException {
+        String keyPath = System.getenv("GOOGLE_APPLICATION_CREDENTIALS");
+        if (keyPath == null || keyPath.isBlank()) {
+            keyPath = "src/main/resources/gcs-key.json"; // fallback 경로
+        }
+        if (keyPath == null || keyPath.isBlank()) {
+            throw new IllegalStateException("GOOGLE_APPLICATION_CREDENTIALS 환경 변수가 설정되지 않았습니다.");
+        }
+
         GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(keyPath));
         return StorageOptions.newBuilder().setCredentials(credentials).build().getService();
     }
