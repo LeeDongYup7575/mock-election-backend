d4a48918dc2bf42884f105ae72756f81dde8d2a2
MIRANG KIM <codekr76@gmail.com>
2025-05-14 14:18:34 +0900

    Feat. 실시간 사용자 목록 표시, 채팅방 참여/퇴장 시 시스템 메시지 추가

--- src/main/java/com/example/mockvoting/domain/chat/entity/Membership.java
+++ src/main/java/com/example/mockvoting/domain/chat/entity/Membership.java
@@ -1,5 +1,6 @@
 package com.example.mockvoting.domain.chat.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -7,13 +8,18 @@ import lombok.NoArgsConstructor;
 
 import java.sql.Date;
 
+@Entity
+@Table(name = "membership") // 테이블 이름 명시
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Membership {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     private String userId;
     private int chatroomId;
     private boolean role;
