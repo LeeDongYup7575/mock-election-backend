9718f8b2a7a5a4675cbbd51a87a75f066f900056
ljh <lonlove0032@gmail.com>
2025-05-21 10:33:49 +0900

    인증절차 수정입니다.

--- src/main/java/com/example/mockvoting/config/SecurityConfig.java
+++ src/main/java/com/example/mockvoting/config/SecurityConfig.java
@@ -46,6 +46,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/polling/**").permitAll()
                         .requestMatchers("/api/youtube/**").permitAll()
                         .requestMatchers("/api/glossary/**").permitAll()
+                        .requestMatchers("/api/policyQuestion/**").permitAll()
                         .requestMatchers(HttpMethod.GET,"/api/community/main").permitAll()
                         .requestMatchers("/api/community/categories/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/community/posts/*/edit").authenticated()
